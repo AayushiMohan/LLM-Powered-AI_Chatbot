b961158ec80ced8c5d0dcfe6afd0d8cf3b91fddb
ByteSpace <aayushimohan118@gmail.com>
2026-08-02 22:59:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-/* eslint-disable no-loop-func */
 import { useState, useRef, useEffect } from "react";
 import "./App.css";
 
-const API_URL = "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 function TypingIndicator() {
   return (
@@ -336,3 +335,4 @@ export default function App() {
     </div>
   );
 }
+  
